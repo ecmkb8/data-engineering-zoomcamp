c80c5cba95e29ba71d82da19001f89f0e0724986
ecmkb8 <ecmkb8@gmail.com>
2024-01-02 14:09:51 -0700

    can't recall

--- week_1_basics_n_setup/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "/Users/evanmoore/gcloud_creds/dtc-de-course-406816-6d128f9b1cca.json"
+  default     = "~/gcloud_creds/dtc-de-course-406816-6d128f9b1cca.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
