329a6faa615809bb2e834e57e1654d9e699852c4
ecmkb8 <ecmkb8@gmail.com>
2024-02-24 17:03:41 -0700

    a staging update

--- 04-analytics-engineering/hw/taxi_rides_ny/models/staging/stg__green_tripdata.sql
+++ 04-analytics-engineering/hw/taxi_rides_ny/models/staging/stg__green_tripdata.sql
@@ -1,34 +1,54 @@
-with source as (
-      select * from {{ source('staging', 'green_tripdata') }}
-),
-renamed as (
+{{ config(materialized= 'view') }}
+
+
+with tripdata as (
+      
+      select 
+      *,
+      row_number() over (partition by vendor_id, lpep_pickup_datetime) as rn 
+      from {{ source('staging', 'green_tripdata') }}
+      where vendor_id is not null
+)
     select
-    "Green" as taxi_type
-    , {{ dbt_utils.generate_surrogate_key(['vendor_id', 'lpep_pickup_datetime']) }} as trip_id
-    , vendor_id
-    , lpep_pickup_datetime as pickup_datetime
-    , lpep_dropoff_datetime as dropoff_datetime
+
+    -- Identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'lpep_pickup_datetime']) }} as tripid 
+    , {{ dbt.safe_cast("vendor_id", api.Column.translate_type("integer")) }} as vendorid
+    , {{ dbt.safe_cast("rate_code_id", api.Column.translate_type("integer")) }} as ratecodeid
+    , {{ dbt.safe_cast("pu_location_id", api.Column.translate_type("integer")) }} as pulocationid
+    , {{ dbt.safe_cast("do_location_id", api.Column.translate_type("integer")) }} as dolocationid
+
+    -- Datetimes
+    , cast( lpep_pickup_datetime as timestamp) as pickup_datetime
+    , cast( lpep_dropoff_datetime as timestamp) as dropoff_datetime
+    
+    -- Trip info
     , store_and_fwd_flag
-    , rate_code_id
-    , pu_location_id
-    , do_location_id
-    , passenger_count
-    , trip_distance
-    , fare_amount
-    , extra
-    , mta_tax
-    , tip_amount
-    , tolls_amount
-    , ehail_fee
-    , improvement_surcharge
-    , total_amount
-    , payment_type
+    , {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count
+    , {{ dbt.safe_cast("trip_distance", api.Column.translate_type("integer")) }} as trip_distance
+    , {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_distance
+
+    -- Fare info
+    , cast( fare_amount as numeric) as fare_amount
+    , cast( extra as numeric) as extra
+    , cast( mta_tax as numeric) as mta_tax
+    , cast( tip_amount as numeric) as tip_amount
+    , cast( tolls_amount as numeric) as tolls_amount
+    , cast( ehail_fee as numeric) as ehail_fee
+    , cast( improvement_surcharge as numeric) as improvement_surcharge
+    , cast( total_amount as numeric) as total_amount
+    , coalesce( 
+        {{ dbt.safe_cast("payment_type"), api.Column.translate_type("integer") }} 
+        , 0
+    ) as payment_type
     , {{ get_payment_type_description('payment_type') }} as payment_type_description
     , congestion_surcharge
     , lpep_pickup_date as pickup_date    
     
-    from source
-)
+    from tripdata
+    where rn = 1
+-- dbt build --m <model.sql> --vars '{ 'is_test_run': false }'
+{%- if var('is_test_run', default=true) %}
+    limit 100
 
-
-select * from renamed
+{% endif -%}
